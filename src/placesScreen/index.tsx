b125c80bd04492ed24caeaed8ec6ae9dd09ab4da
J-HOVELAQUE <j.hovelaque@orange.fr>
2021-08-08 17:49:25 +0200

    get places on place screen

--- src/placesScreen/index.tsx
+++ src/placesScreen/index.tsx
@@ -1,6 +1,34 @@
 import CardPlace from "./components/CardPlace";
+import { useEffect, useState } from "react";
+
+const endPoint: string = "localhost:3000";
+
+interface Place {
+  id: string;
+  name: string;
+  altitudeInMeters: number;
+  mountainLocation: string;
+  city?: string;
+  picture?: string;
+}
 
 export default function PlacesScreen(props) {
+  const [places, setPlaces] = useState([]);
+
+  useEffect(() => {
+    async function getPlaces() {
+      const rawAnswer = await fetch("http://localhost:3000/place", {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      });
+      const answer = await rawAnswer.json();
+      setPlaces(answer.places);
+    }
+    getPlaces();
+  }, []);
   return (
     <>
       <h1 className="hikingTitle">Lieux de randonnée</h1>
