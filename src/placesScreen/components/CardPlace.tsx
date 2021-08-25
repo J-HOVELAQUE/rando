e7ecc3036d2d0f7b605d7b18399fca47610069f2
J-HOVELAQUE <j.hovelaque@orange.fr>
2021-08-25 10:57:01 +0200

    overlay on place card

--- src/placesScreen/components/CardPlace.tsx
+++ src/placesScreen/components/CardPlace.tsx
@@ -1,5 +1,9 @@
 import { Card } from "react-bootstrap";
 import "./cardStyle.css";
+import Popover from "react-bootstrap/Popover";
+import PopoverBody from "react-bootstrap/PopoverBody";
+import PopoverHeader from "react-bootstrap/PopoverHeader";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
 
 export interface Place {
   _id?: string;
@@ -14,9 +18,30 @@ export default function CardPlace(props) {
   const placeData: Place = props.placeData;
   const pictureUrl: string = placeData.picture || "/montain_default.jpg";
 
+  const popover = (
+    <Popover id="popover-basic">
+      <Popover.Header as="h3">Popover right</Popover.Header>
+      <Popover.Body>
+        And here's some <strong>amazing</strong> content. It's very engaging.
+        right?
+      </Popover.Body>
+    </Popover>
+  );
+
   return (
     <Card className="card-place">
-      <Card.Img variant="top" src={pictureUrl} className="card-place-picture" />
+      <OverlayTrigger
+        trigger="click"
+        placement="auto"
+        overlay={popover}
+        delay={1000}
+      >
+        <Card.Img
+          variant="top"
+          src={pictureUrl}
+          className="card-place-picture"
+        />
+      </OverlayTrigger>
       <Card.Body>
         <Card.Title bsPrefix="card-place-title">{placeData.name}</Card.Title>
         <Card.Text>Massif: {placeData.mountainLocation}</Card.Text>
