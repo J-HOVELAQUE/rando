da9c46b26a894155414f89a4daf6cd2fab7bbd9c
J-HOVELAQUE <j.hovelaque@orange.fr>
2021-08-25 18:43:13 +0200

    fix issue with react navigation

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,11 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { createStore, combineReducers, Store } from "redux";
 import { Provider } from "react-redux";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./home";
+import HikingSheet from "./hikingSheet";
+import PlacesScreen from "./placesScreen";
+import ParticipantsScreen from "./participantsScreen";
 
 import RandoNavBar from "./randoNavBar/RandoNavBar";
 
@@ -12,7 +17,15 @@ const store: Store = createStore(combineReducers({ activeHike }));
 function App() {
   return (
     <Provider store={store}>
-      <RandoNavBar />
+      <Router>
+        <RandoNavBar />
+        <Switch>
+          <Route exact={true} path="/" component={Home} />
+          <Route path="/rando" component={HikingSheet} />
+          <Route path="/lieux" component={PlacesScreen} />
+          <Route path="/participants" component={ParticipantsScreen} />
+        </Switch>
+      </Router>
     </Provider>
   );
 }
--- src/randoNavBar/RandoNavBar.tsx
+++ src/randoNavBar/RandoNavBar.tsx
@@ -1,6 +1,7 @@
 import { Navbar, Nav } from "react-bootstrap";
 import "./RandoNavBar.css";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import Home from "../home";
 import PlacesScreen from "../placesScreen";
@@ -10,35 +11,34 @@ import ParticipantsScreen from "../participantsScreen";
 export default function RandoNavBar(props) {
   return (
     <>
-      <BrowserRouter>
-        <Navbar variant="dark" className="nav-container-rando">
-          <Nav.Link href="/" className="nav-item-rando">
-            Home
-          </Nav.Link>
-          <Nav.Link href="/lieux" className="nav-item-rando">
-            Lieux
-          </Nav.Link>
-          <Nav.Link href="/rando" className="nav-item-rando">
-            Rando
-          </Nav.Link>
-          <Nav.Link href="/participants" className="nav-item-rando">
-            Participants
-          </Nav.Link>
-          <Nav.Link href="#" className="nav-item-rando">
-            Connexion
-          </Nav.Link>
-        </Navbar>
+      <Navbar variant="dark" className="nav-container-rando">
+        {/* <Nav.Link href="/" className="nav-item-rando"> */}
+        <Link to="/" className="nav-item-rando">
+          Home
+        </Link>
+        {/* </Nav.Link> */}
+        {/* <Nav.Link className="nav-item-rando"> */}
+        <Link to="/lieux" className="nav-item-rando">
+          Lieux
+        </Link>
+        {/* </Nav.Link> */}
+        {/* <Nav.Link href="/rando" className="nav-item-rando"> */}
+        <Link to="/rando" className="nav-item-rando">
+          Rando
+        </Link>
+        {/* </Nav.Link> */}
+        {/* <Nav.Link href="/participants" className="nav-item-rando"> */}
+        <Link to="/participants" className="nav-item-rando">
+          Participants
+        </Link>
+        {/* </Nav.Link> */}
+        <Nav.Link href="#" className="nav-item-rando">
+          Connexion
+        </Nav.Link>
+      </Navbar>
 
-        <Switch>
-          <Route exact={true} path="/" component={Home} />
-          <Route path="/rando" component={HikingSheet} />
-          <Route path="/lieux" component={PlacesScreen} />
-          <Route path="/participants" component={ParticipantsScreen} />
-
-          {/* Not Found */}
-          <Route component={() => <Redirect to="/" />} />
-        </Switch>
-      </BrowserRouter>
+      {/* Not Found */}
+      <Route component={() => <Redirect to="/" />} />
     </>
   );
 }
